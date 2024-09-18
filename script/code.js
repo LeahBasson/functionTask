b6a05fed56faef67e2716d9e053e22af3177e8d5
LeahBasson <leahbasson54@gmail.com>
2024-09-18 08:40:13 +0200

    function task

---
+++ script/code.js
@@ -0,0 +1,21 @@
+//Question 1
+function greet(value){
+    console.log(value);
+}
+
+greet('Hello');
+
+//Question 2
+function multiply(numb1, numb2){
+    console.log(numb1*numb2);
+}
+
+multiply(2,2)
+
+//Question 3
+function tableOfFive(num){
+   return table = num*5;  
+}
+
+tableOfFive(5);
+console.log(table);
